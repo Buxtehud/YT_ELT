22bec0b14f74941b3048958164e4b41fdd8e3cce
Andres Andrade <andres.anreyes@gmail.com>
2026-01-15 23:50:00 -0300

    added write to json file

--- video_stats.py
+++ video_stats.py
@@ -1,7 +1,9 @@
 import requests
 import json
 import os
+from datetime import date
 from dotenv import load_dotenv
+
 
 load_dotenv(dotenv_path="./.env")
 
@@ -108,9 +110,15 @@ def extract_video_data(video_ids):
         raise e
 
 
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
    playlist_id = get_playlist_id()
    video_ids = get_video_ids(playlist_id)
-   extract_video_data(video_ids)
+   video_data = extract_video_data(video_ids)
+   save_to_json(video_data)
