09c7f678e86a5d750c32577a8d88cc2589466e94
Andres Andrade <andres.anreyes@gmail.com>
2026-01-26 15:18:22 -0300

    Added functional tests

--- dags/main.py
+++ dags/main.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.models import Variable
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from zoneinfo import ZoneInfo
 from datetime import datetime, timedelta, date
 import json
@@ -12,7 +13,7 @@ from dataquality.soda import yt_elt_data_quality
 # Define the local timezone
 local_tz = ZoneInfo("America/Buenos_Aires")
 
-# # Default Args
+# Default Args
 default_args = {
     "owner":"andy",
     "depends_on_past": False,
@@ -24,6 +25,9 @@ default_args = {
     # "end_date": datetime(2030, 12, 31, tzinfo=local_tz)
 }
 
+# Variables
+staging_schema = "staging"
+core_schema = "core"
 
 
 with DAG(
@@ -32,7 +36,7 @@ with DAG(
     description = 'DAG to produce JSON file with raw data',
     schedule = '0 14 * * *',
     catchup = False
-) as dag:
+) as dag_produce:
     
     # Define tasks
     playlist_id = get_playlist_id()
@@ -40,29 +44,42 @@ with DAG(
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
 
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db"
+    )
+
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
+
 
 with DAG(
     dag_id = 'update_db',
     default_args = default_args,
     description = 'DAG to process JSON file and insert data into both staging and core schemas',
-    schedule = '0 15 * * *',
+    schedule = None,
     catchup = False
-) as dag:
+) as dag_update:
     
     # Define tasks
     update_staging_table = staging_table()
     update_core_table = core_table()
 
-    update_staging_table >> update_core_table
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality"
+    )
+
+    update_staging_table >> update_core_table >> trigger_data_quality
+
+
 
 with DAG(
     dag_id = 'data_quality',
     default_args = default_args,
     description = 'DAG to check the data quality on both layers in the db',
-    schedule = '0 16 * * *',
+    schedule = None,
     catchup = False
-) as dag:
+) as dag_quality:
     
     # Define tasks
     soda_validate_staging = yt_elt_data_quality("staging")
